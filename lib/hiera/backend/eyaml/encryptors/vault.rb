5d6883de03dcbd31bdbe1ff70108ce6cadcd4307
Craig Dunn <craig@craigdunn.org>
2019-11-21 10:45:48 +0100

    release 1.0.1

--- lib/hiera/backend/eyaml/encryptors/vault.rb
+++ lib/hiera/backend/eyaml/encryptors/vault.rb
@@ -14,7 +14,7 @@ class Hiera
           end
 
 
-          VERSION = "1.0.0"
+          VERSION = "1.0.1"
           HTTP_HANDLER = Hiera::Backend::Eyaml::Encryptors::Vault::Httphandler
 
           self.tag = 'VAULT'
